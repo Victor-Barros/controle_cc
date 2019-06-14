4f38f9f44d9a5bf65ffbdd7f96fb0f76221f3016
Victor-Barros <victorbarroscoch@gmail.com>
2019-06-14 09:22:23 -0300

    fix sistema de captura 100hz

--- controle_cc.ino
+++ controle_cc.ino
@@ -3,8 +3,9 @@
 const int canal_a=2, canal_b=3;
 int i=0;
 volatile long pulsos;
-volatile double x1[2]={0,0},x2[2]={0,0},ts=0.002,wc=50,u=0;
-int serial_mode=0; //0-serial print 1-serial data
+volatile double x1[2]={0,0},x2[2]={0,0},ts=0.002,wc=50,u=0,soma=0;
+volatile int cont_soma=0;
+int serial_mode=0; //0-serial print; 1-serial data
 String str;
 
 void setup() {
@@ -20,11 +21,12 @@ void loop() {
 
   if(Serial.available() > 0) {
     str=Serial.readString();
-    if (str == "c") serial_mode=1;
+    if (str == "c\n") serial_mode=1;
+    else if (str == "p\n") serial_mode=2;
     else serial_mode=0;
   }
   
-  if(millis()%100==0 && serial_mode == 0) {
+  if(millis()%100==0 && serial_mode == 2) {
     Serial.println((x2[1]*60)/(2*PI));
   }
 }
@@ -44,7 +46,15 @@ void svf() {
   u=(PI/16)*pulsos;
   x1[1]=x2[0]*ts+x1[0];
   x2[1]=x2[0]*(1-2*wc*ts)-x1[0]*wc*wc*ts+u*wc*wc*ts;
-  if (serial_mode == 1) Serial.println(x2[1]);
+  if (serial_mode == 1) {
+    soma+=x2[1];
+    cont_soma++;
+    if(cont_soma >= 5) {
+      Serial.println(soma/5);
+      soma=0;
+      cont_soma=0;
+    }
+  }
   interrupts();
 }
 
